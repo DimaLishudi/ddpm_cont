32548459fbc1555a4a5b6601ce44f7786c2a3fb1
Dmitry Lishudi <lishudi.dmitry@gmail.com>
2023-04-20 06:47:17 +0300

    first commit

---
+++ ddpm_sde.py
@@ -0,0 +1,123 @@
+import torch
+import numpy as np
+
+
+class DDPM_SDE:
+    def __init__(self, config):
+        """Construct a Variance Preserving SDE.
+
+        Args:
+          beta_min: value of beta(0)
+          beta_max: value of beta(1)
+          N: number of discretization steps
+        """
+        self.N = config.sde.N
+        self.beta_0 = config.sde.beta_min
+        self.beta_1 = config.sde.beta_max
+
+    @property
+    def T(self):
+        return 1
+
+    def sde(self, x, t):
+        """
+        Calculate drift coeff. and diffusion coeff. in forward SDE
+        """
+        beta = (self.beta_1 - self.beta_0)*t + self.beta_0 # linear beta(t)
+        drift = -beta * x / 2
+        diffusion = torch.sqrt(beta)
+        return drift, diffusion
+
+    def marginal_prob(self, x_0, t):
+        """
+        Calculate marginal q(x_t|x_0)'s mean and std
+        """
+        B = (self.beta_1 - self.beta_0) * t**2/2 + self.beta_0 * t
+        dims = [1] * (x_0.dim()-1)
+        B = B.view(-1, *dims) # unsqueeze B to have same num of dims as x_0
+        mean = torch.exp(-B/2) * x_0
+        std  = torch.sqrt(1-torch.exp(-B))
+        return mean, std
+    
+    def marginal_std(self, t):
+        """
+        Calculate marginal q(x_t|x_0)'s std
+        """
+        B = (self.beta_1 - self.beta_0) * t**2/2 + self.beta_0 * t
+        std = torch.sqrt(1-torch.exp(-B))
+        return std
+
+    def prior_sampling(self, shape):
+        return torch.randn(*shape)
+
+    def reverse(self, score_fn, ode_sampling=False):
+        """Create the reverse-time SDE/ODE.
+        Args:
+          score_fn: A time-dependent score-based model that takes x and t and returns the score.
+          ode_sampling: If `True`, create the reverse-time ODE used for probability flow sampling.
+        """
+        N = self.N
+        T = self.T
+        sde_fn = self.sde
+
+        # Build the class for reverse-time SDE.
+        class RSDE:
+            def __init__(self):
+                self.N = N
+                self.ode_sampling = ode_sampling
+
+            @property
+            def T(self):
+                return T
+
+            def sde(self, x, t, y=None):
+                """
+                Create the drift and diffusion functions for the reverse SDE/ODE.
+                
+                
+                y is here for class-conditional generation through score SDE/ODE
+                """
+                
+                """
+                Calculate drift and diffusion for reverse SDE/ODE
+                
+                
+                ode_sampling - True -> reverse SDE
+                ode_sampling - False -> reverse ODE
+                """
+                # forward sde coefficients
+                drift, diffusion = sde_fn(x, t)
+                if self.ode_sampling:
+                    # -forward langevin:
+                    drift -= diffusion**2 * score_fn(x, t, y) / 2
+                    diffusion = 0
+                else:
+                    # -2*forward langevin (diffusion doesn't change)
+                    drift -= diffusion**2 * score_fn(x, t, y)
+                return drift, diffusion
+        return RSDE()
+
+
+class EulerDiffEqSolver:
+    def __init__(self, sde, score_fn, ode_sampling = False):
+        self.sde = sde
+        self.score_fn = score_fn
+        self.ode_sampling = ode_sampling
+        self.rsde = sde.reverse(score_fn, ode_sampling)
+
+    def step(self, x, t, y=None):
+        """
+        Implement reverse SDE/ODE Euler solver
+        """
+        
+        
+        """
+        x_mean = deterministic part
+        x = x_mean + noise (yet another noise sampling)
+        """
+        dt = self.rsde.T / self.rsde.N
+        drift, diffusion = self.rsde(x, t, y)
+        dw = torch.randn_like(x) * np.sqrt(dt)
+        x_mean = x + drift * dt
+        x = x_mean + diffusion * dw
+        return x, x_mean
